523ea0fbdf38ef292d92f41c10ae02da7261643b
Shamura Ahmad <47661479+animshamura@users.noreply.github.com>
2023-05-28 22:27:18 +0600

    Create  1004 - Simple Product.kt

---
+++ 1004 - Simple Product.kt
@@ -0,0 +1,8 @@
+import java.util.*;
+fun main(args: Array<String>) {
+  val red = Scanner(System.`in`)
+  var a:Int = red.nextInt()
+  var b:Int = red.nextInt()
+  print("PROD = ")
+  println(a*b)
+}
