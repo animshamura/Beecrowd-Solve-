d35ab6ac7600aa7338b90b79924a57ac35f9adf6
Shamura Ahmad <47661479+animshamura@users.noreply.github.com>
2023-05-28 22:26:35 +0600

    Create  1005 - Average 1.kt

---
+++ 1005 - Average 1.kt
@@ -0,0 +1,8 @@
+import java.util.*;
+fun main(args: Array<String>) {
+  val red = Scanner(System.`in`)
+  var a:Double = red.nextDouble()
+  var b:Double = red.nextDouble()
+  print("MEDIA = ")
+  println("%.5f".format((a*3.5+b*7.5)/11.0))
+}
