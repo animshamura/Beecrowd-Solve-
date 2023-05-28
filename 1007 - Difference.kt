0495318d6ad2c78b8d7c20e02dd412d1090fb3a4
Shamura Ahmad <47661479+animshamura@users.noreply.github.com>
2023-05-28 22:25:52 +0600

    Create  1007 - Difference.kt

---
+++ 1007 - Difference.kt
@@ -0,0 +1,10 @@
+import java.util.*;
+fun main(args: Array<String>) {
+  val red = Scanner(System.`in`)
+  var a:Int = red.nextInt()
+  var b:Int = red.nextInt()
+  var c:Int = red.nextInt()
+  var d:Int = red.nextInt()
+  var e:Int = a*b-c*d 
+  println("DIFERENCA = "+e)
+}
