22d1281a1c410be5b6d5b273a4389f5dfbaccb85
Shamura Ahmad <47661479+animshamura@users.noreply.github.com>
2023-05-28 22:27:54 +0600

    Create 1002 - Area of a Circle.kt

---
+++ 1002 - Area of a Circle.kt
@@ -0,0 +1,9 @@
+import java.util.*;
+fun main(args: Array<String>) {
+  val red = Scanner(System.`in`)
+  var r:Double = red.nextDouble()
+  var x:Double = 3.14159
+  var a:Double = x*r*r
+  print("A=")
+  println("%.4f".format(a))
+}
