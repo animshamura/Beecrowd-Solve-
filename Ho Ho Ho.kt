879b2954710f9fe0eb30ec43c1ce5828254801a9
Shamura Ahmad <47661479+animshamura@users.noreply.github.com>
2023-05-28 22:23:38 +0600

    Create Ho Ho Ho.kt

---
+++ Ho Ho Ho.kt
@@ -0,0 +1,10 @@
+import java.util.*;
+fun main(args: Array<String>) {
+    var t:Int = readLine()!!.toInt()
+    var i:Int = 1
+    while(i<=t){
+        if(i==t) println("Ho!")
+        else print("Ho ")
+        i+=1
+    }
+    }
